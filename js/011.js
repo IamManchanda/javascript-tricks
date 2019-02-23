55bc60fcac8357a5b60aaae173c78de05b228672
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-02-23 18:45:25 +0530

    Get a random element from an array.

---
+++ js/011.js
@@ -0,0 +1,7 @@
+console.clear();
+
+{ /* Get a random element from an array.*/
+  const ages = [12, 32, 98, 45, 38, 76];
+  const randomAge = ages[Math.floor(Math.random() * ages.length)];
+  console.log(randomAge);
+};
