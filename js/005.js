c84c7753e55d1631f1725629d3ff52df0e3573ff
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-02-23 14:56:01 +0530

    Feat: Prevent `new` Object Properties from being added (not extensible).

---
+++ js/005.js
@@ -0,0 +1,21 @@
+console.clear();
+
+/* Prevent `new` Object Properties from being added (not extensible). */
+
+var person = {
+  firstname: 'Harman',
+  middlename: 'Singh',
+  lastname: 'Manchanda',
+  nickname: 'Harry',
+  age: 26,
+  email: 'harmanmanchanda182@gmail.com'
+};
+
+Object.preventExtensions(person);
+
+const isPersonExtensible = Object.isExtensible(person);
+person.isBad = true;
+
+console.log({ isPersonExtensible, person }); // No `isBad` property as the object is not extensible
+
+
