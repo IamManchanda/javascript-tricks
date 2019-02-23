10274791ac72990f7ca25227d076ed7092c2d64b
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-02-23 14:09:56 +0530

    Feat: Get Length of an object

---
+++ js/001.js
@@ -0,0 +1,11 @@
+console.clear();
+
+/* Get Length of an object */
+
+const arr = [1, 2, 3];
+const arrLength = arr.length;
+console.log({ arrLength });
+
+const obj = { a: 1, b: 2, c: 3 };
+const objLength = Object.keys(obj).length;
+console.log({ objLength });
