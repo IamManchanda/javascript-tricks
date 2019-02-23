1be48276fe0ef677f3348c356d6930b784340c38
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-02-24 00:31:18 +0530

    Changing Length of Decimals

--- js/022.js
+++ js/022.js
@@ -2,13 +2,14 @@ console.clear();
 
 { /* Measure time it takes to execute a function.*/
   const getUserData = async (username) => await fetch(`https://api.github.com/users/${username}`);
+
+  console.log('--------Solution--------');
   const timeTakenWrapper = (fn, ...args) => {
     console.time('timeTakenWrapper');
     let result = fn(...args);
     console.timeEnd('timeTakenWrapper')
     return result;
   };
-  
   const apiResult = timeTakenWrapper(getUserData, 'IamManchanda');
   console.log({ apiResult });
 };
---
+++ js/023.js
@@ -0,0 +1,9 @@
+console.clear();
+
+{ /* Changing Length of Decimals.*/
+  const num = 3.14159;
+  
+  console.log('--------Solution--------');
+  const fixedNum = Number(num.toFixed(2));
+  console.log({ fixedNum });
+};
