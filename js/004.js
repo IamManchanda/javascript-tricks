d7bce731b2f33aa13002f752e9e14434ec399104
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-02-23 14:47:50 +0530

    Feat: Check if a property exists in object.

---
+++ js/004.js
@@ -0,0 +1,21 @@
+console.clear();
+
+/* Check if a property exists in object. */
+
+var person = {
+  firstname: 'Harman',
+  middlename: 'Singh',
+  lastname: 'Manchanda',
+  nickname: 'Harry',
+  age: 26,
+  address: {
+    street: 'C-3 Block',
+    territory: 'Delhi',
+    country: 'India'
+  },
+};
+
+const checkFirstnameInPerson = 'firstname' in person;
+const checkStreetInPerson = 'street' in person;
+const checkStreetInPersonsAddress = 'street' in person.address;
+console.log({ checkFirstnameInPerson, checkStreetInPerson, checkStreetInPersonsAddress });
