375da526f0239c07518f0f675981b1962f884a99
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-02-23 20:16:35 +0530

    Filter out Difference (non shared items) Between Two Arrays but not duplicates

--- js/017.js
+++ js/017.js
@@ -1,12 +1,17 @@
 console.clear();
 
-{ /* Find Difference (non shared items) Between Two Arrays but not duplicates.*/
-  const arr1 = ['fred', 'doug', 'amy', 'amy'];
-  const arr2 = ['fred', 'doug', 'joe', 'joe'];
+{ /* Filter out Difference (non shared items) Between Two Arrays but not duplicates.*/
+  const group1 = ['fred', 'doug', 'amy', 'amy'];
+  const group2 = ['fred', 'doug', 'joe', 'joe'];
 
-  const nonSharedItems = [
-    ...[...new Set(arr1)].filter(el => !arr2.includes(el)),
-    ...[...new Set(arr2)].filter(el => !arr1.includes(el)),
-  ];
+  console.log('--------Solution--------');
+  const filterDifference = (arr1, arr2) => ({
+    [arr1.ref]: [...new Set(arr1.values)].filter(el => !arr2.values.includes(el)),
+    [arr2.ref]: [...new Set(arr2.values)].filter(el => !arr1.values.includes(el)),
+  });
+  const nonSharedItems = filterDifference(
+    { ref: 'group1', values: group1 }, 
+    { ref: 'group2', values: group2 },
+  );
   console.log({ nonSharedItems });
 };
