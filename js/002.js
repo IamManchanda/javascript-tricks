15b7e8a640f2b0f9c9c864a4bb873300816b3c78
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-02-23 14:26:22 +0530

    Chore!

--- js/002.js
+++ js/002.js
@@ -2,27 +2,28 @@ console.clear();
 
 /* Iterating over an object (Accessing both keys and values) */
 
-const name = {
+const person = {
   firstname: 'Harman',
   middlename: 'Singh',
   lastname: 'Manchanda',
   nickname: 'Harry',
+  age: 26,
 };
 
 console.log('--------First Solution with `forin`--------');
-for (const key in name) {
-  if (name.hasOwnProperty(key)) {
-    const value = name[key];
+for (const key in person) {
+  if (person.hasOwnProperty(key)) {
+    const value = person[key];
     console.log(`User's ${key} is ${value}`);
   }
 }
 
 console.log('--------Second Solution with `forof`--------');
-for (const [key, value] of Object.entries(name)) {
+for (const [key, value] of Object.entries(person)) {
   console.log(`User's ${key} is ${value}`);
 }
 
 console.log('--------Third Solution with `forEach`--------');
-Object.entries(name).forEach(([key, value]) => {
+Object.entries(person).forEach(([key, value]) => {
   console.log(`User's ${key} is ${value}`);
 });
